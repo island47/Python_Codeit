6a4ce5e956d92f70fa365c86f1b57208c3a5fdbe
jiwookYG <island0318@gmail.com>
2020-11-12 04:54:43 +0900

    python list

---
+++ example.py
@@ -0,0 +1,7 @@
+def in_list(some_list, value):
+    i = 0
+    while i < len(some_list):
+        # some_list에서 value를 찾으면 True를 리턴
+        if some_list[i] == value:
+            return True
+        i = i + 1
